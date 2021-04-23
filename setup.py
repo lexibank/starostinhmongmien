afc3de7227575a1013605a8c7adbfd66406e6e05
chrzyki <christoph@functor-argument.net>
2021-04-23 09:27:02 +0200

    Update scaffolding, update metadata

--- setup.py
+++ setup.py
@@ -7,19 +7,13 @@ with open("metadata.json", encoding="utf-8") as fp:
 
 
 setup(
-    name='lexibank_starostinhmongmien',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_starostinhmongmien'],
+    name="lexibank_starostinhmongmien",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_starostinhmongmien"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'starostinhmongmien=lexibank_starostinhmongmien:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.1',
-    ]
+    entry_points={"lexibank.dataset": ["starostinhmongmien=lexibank_starostinhmongmien:Dataset"]},
+    install_requires=["pylexibank>=3.0"],
 )
